bfa89ce36185b416070a09ce2e488aa0776222b8
Manoel Dias <nick.lp@hotmail.com>
2019-06-07 13:54:44 -0300

    Criado envio de email forgot

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -15,7 +15,7 @@ class Mailer{
 	public function __construct($toAdress, $toName, $subject, $tplName, $data = array()){
 
 		$config = array(
-					"tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].'/views/email',
+					"tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].'/views/email/',
 					"cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 					"debug"         => false
 				);
@@ -31,59 +31,67 @@ class Mailer{
 		$html = $tpl->draw($tplName, true);
 
 
-		$this->$mail = new \PHPMailer;
+		$this->mail = new \PHPMailer;
 
 		//Tell PHPMailer to use SMTP
-		$this->$mail->isSMTP();
+		$this->mail->isSMTP();
 
 		//Enable SMTP debugging
 		// 0 = off (for production use)
 		// 1 = client messages
 		// 2 = client and server messages
-		$this->$mail->SMTPDebug = 0;
+		$this->mail->SMTPDebug = 0;
 
 		//Ask for HTML-friendly debug output
-		$this->$mail->Debugoutput = 'html';
+		$this->mail->Debugoutput = 'html';
 
 		//Set the hostname of the mail server
-		$this->$mail->Host = 'smtp.gmail.com';
+		$this->mail->Host = 'smtp.gmail.com';
 		// use
 		// $mail->Host = gethostbyname('smtp.gmail.com');
 		// if your network does not support SMTP over IPv6
 
 		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-		$this->$mail->Port = 587;
+		$this->mail->Port = 587;
+
+		$this->mail->SMTPOptions = array(
+			"ssl"=>array(
+				"verify_peer"=>false,
+				"verify_peer_name"=>false,
+				"allow_self_signed"=>true
+			)
+		);
 
 		//Set the encryption system to use - ssl (deprecated) or tls
-		$this->$mail->SMTPSecure = 'tls';
+		$this->mail->SMTPSecure = 'tls';
 
 		//Whether to use SMTP authentication
-		$this->$mail->SMTPAuth = true;
+		$this->mail->SMTPAuth = true;
 
 		//Username to use for SMTP authentication - use full email address for gmail
-		$this->$mail->Username = Mailer::USERNAME;
+		$this->mail->Username = Mailer::USERNAME;
 
 		//Password to use for SMTP authentication
-		$this->$mail->Password = Mailer::PASSWORD;
+		$this->mail->Password = Mailer::PASSWORD;
 
 		//Set who the message is to be sent from
-		$this->$mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
 
 		//Set an alternative reply-to address
 		//$mail->addReplyTo();
 
 		//Set who the message is to be sent to
-		$this->$mail->addAddress($toAdress, $toName);
+		$this->mail->addAddress($toAdress, $toName);
 
 		//Set the subject line
-		$this->$mail->Subject = $subject;
+		$this->mail->Subject = $subject;
 
 		//Read an HTML message body from an external file, convert referenced images to embedded,
 		//convert HTML into a basic plain-text alternative body
-		$this->$mail->msgHTML($html);
+		$this->mail->msgHTML($html);
 
 		//Replace the plain text body with one created manually
-		$this->$mail->AltBody = 'This is a plain-text message body';
+		$this->mail->AltBody = 'This is a plain-text message body';
 
 		//Attach an image file
 		//$this->$mail->addAttachment('images/phpmailer_mini.png');
@@ -94,7 +102,7 @@ class Mailer{
 
 	public function send(){
 
-		return $this->$mail->send();
+		return $this->mail->send();
 	}
 }
 
